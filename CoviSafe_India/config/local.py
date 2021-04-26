52e9cf08f4415204d10c09059046fd7c545815fd
BadduCoder <sumit7engineer@gmail.com>
2021-04-27 02:41:42 +0530

    Add ALLOWED_HOST to local too

--- CoviSafe_India/config/local.py
+++ CoviSafe_India/config/local.py
@@ -1,4 +1,6 @@
+from decouple import config
+
 DEBUG = True
-ALLOWED_HOST = []
+ALLOWED_HOST = [config('PROD_HOST'),]
 
 SECRET_KEY = 'django-insecure-_bgavy588xn=v69=#cn*#b#$7_et6gqp2(8v@+mjbsj)4%-yis'
